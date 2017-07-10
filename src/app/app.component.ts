420fde5d0ccb49f6212c7e1e19c84f59c92665ce
hambardzumyan-mane <hambardzumyan.mane@gmail.com>
2017-07-11 00:08:46 +0400

    Separate the page from app-navigation-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<app-navigation-bar></app-navigation-bar>`,
+  template: `<app-navigation-bar></app-navigation-bar>`, // Good idea but why only the nav-bar. Where is your page?
   //templateUrl: './app.component.html',
 })
 export class AppComponent {
